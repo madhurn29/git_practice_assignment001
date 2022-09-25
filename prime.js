eb5ddeb3c75085ae8c58d16bb8c2e8ef10b6b388
soumyah1512 <99666938+soumyah1512@users.noreply.github.com>
2022-09-25 13:27:40 +0530

    update

--- prime.js
+++ prime.js
@@ -1,9 +1,13 @@
 function checkPrime(num){
-    
+    let count = 0
     for(let i=1;i<=num;i++){
         if(num%i==0){
-            return true;
+            count++
         }
+    }
+    if(count === 2)
+    {
+        return true;
     }
     return false;
 }
